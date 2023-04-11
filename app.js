f4e2485d0829a1066278ca9ce4da3f4a9d450fb4
Nikita Agrawal <nikiagrawalllg@gmail.com>
2023-04-12 02:18:58 +0530

    first commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.use(express.json());
 app.use(express.static("public"));
 app.use(express.urlencoded({extended:true}));
 app.get("/",function(req,res){
-    res.sendFile(__dirname+"/signup.html");
+    res.sendFile(__dirname+"/frontend/signup.html");
 });
 app.post("/",function(req,res){
     const first=req.body.f;
@@ -25,16 +25,16 @@ app.post("/",function(req,res){
         ]
     }
     const jsonData=JSON.stringify(data);
-    const url="https://us8.api.mailchimp.com/3.0/lists/9c3e5bbb3a";
+    const url="https://us8.api.mailchimp.com/3.0/lists/8c3e5bbb3a";
     const options={
         method:"POST",
         auth:"nikita:218b272ca2ebc4dfecf90aff46016c97-us8"
     }
     const request=https.request(url,options,function(response){
         if(response.statusCode===200){
-            res.sendFile(__dirname+"/success.html");
+            res.sendFile(__dirname+"/frontend/success.html");
         } else{
-            res.sendFile(__dirname+"/failure.html");
+            res.sendFile(__dirname+"/frontend/failure.html");
         }
         response.on("data",function(data){
             console.log(JSON.parse(data));
